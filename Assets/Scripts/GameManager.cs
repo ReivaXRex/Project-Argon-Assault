87dac6fe4c655dd9ed6ca26bf9c778ac510ab788
Xavier Faria <xavierfaria@outlook.com>
2021-01-30 21:41:41 -0500

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadLevel", 2f);
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
